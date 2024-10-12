e863607b067b9bdccd3f1ceb8fa2549d203ac0e7
hottestdog <hottestdog@proton.me>
2024-10-12 10:42:36 +0100

    :sparkles: Add SEO data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,6 +16,7 @@ import { Button } from "~/components/ui/button";
 import Image from "next/image";
 import { RaydiumPoolInfoResponse } from "~/types/api";
 import Link from "next/link";
+import { Metadata } from "next";
 
 /**
  * 
@@ -39,6 +40,47 @@ const getPrice = async () => {
   const response = await fetch(url);
   const data: RaydiumPoolInfoResponse = await response.json();
   return data?.data?.[0]?.price;
+};
+
+export const metadata: Metadata = {
+  title: "Hottest Dog Token | Help this dog cool the F*CK down!",
+  description:
+    "Discover the Hottest Dog Token: A unique cryptocurrency project aimed at supporting dog charities in warm climates. Join our community and make a difference!",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://hottestdog.org/",
+    siteName: "Hottest Dog Token",
+    images: [
+      {
+        url: "https://hottestdog.org/share-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Hottest Dog Token",
+      },
+    ],
+  },
+  keywords: [
+    "Hottest Dog Token",
+    "cryptocurrency",
+    "dog charity",
+    "warm climates",
+    "community token",
+    "Solana ecosystem",
+  ],
+  authors: [{ name: "Hottest Dog Token Team" }],
+  category: "Cryptocurrency",
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
 };
 
 export default function Home() {
